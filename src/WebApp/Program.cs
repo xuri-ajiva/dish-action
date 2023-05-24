510f3bd4a204a5b10ef2c61d123c25041d9ae5bd
Tobias Durst <t0bl4s.durst@gmail.com>
2023-05-24 17:40:56 +0200

    port dish-web

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using WebApp;
+using WebApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient {
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+builder.Services.AddAntDesign();
+builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddScoped<DialogService>();
+
+builder.Services.AddScoped<DishDashDataService>();
 
 await builder.Build().RunAsync();
---
+++ src/WebApp/Services/DishDashDataService.cs
@@ -0,0 +1,52 @@
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Json;
+
+namespace WebApp.Services;
+
+public class DishDashDataService
+{
+    private readonly HttpClient _client;
+    private List<Day>? _days;
+    private List<Image>? _images;
+    private List<Meal>? _meals;
+    private List<string>? _sources;
+    public DishDashDataService(HttpClient client) => _client = client;
+
+    [MemberNotNull(nameof(_days), nameof(_images), nameof(_meals))]
+    public async Task TryFetchDataAsync()
+    {
+#pragma warning disable CS8774
+        if (HasData) return;
+#pragma warning restore CS8774
+        await FetchInternal();
+    }
+    [MemberNotNull(nameof(_days), nameof(_images), nameof(_meals))]
+    private async Task FetchInternal()
+    {
+        _days = await _client.GetFromJsonAsync<List<Day>>("data/days.json") ?? new List<Day>();
+        _images = await _client.GetFromJsonAsync<List<Image>>("data/images.json") ?? new List<Image>();
+        _meals = await _client.GetFromJsonAsync<List<Meal>>("data/meals.json") ?? new List<Meal>();
+        HasData = true;
+    }
+
+    public bool HasData { get; private set; }
+
+    public async Task<List<string>> GetAllSources()
+    {
+        await TryFetchDataAsync();
+        return _sources ??= _days.Select(x => x.Source).Distinct().ToList();
+    }
+
+    public async Task<List<Day>> GetDayByRange(DateOnly start, DateOnly end)
+    {
+        await TryFetchDataAsync();
+        return _days.Where(x => x.Date >= start && x.Date <= end).ToList();
+    }
+
+    public async Task<List<Meal>> GetAllMeals()
+    {
+        await TryFetchDataAsync();
+        return _meals.ToList();
+    }
+}
