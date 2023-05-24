d6884c39576fd8bff7ee5db2e1154c31092183b6
Tobias Durst <t0bl4s.durst@gmail.com>
2023-05-24 12:38:50 +0200

    Src Init

---
+++ src/Scraper/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+using var client = new HttpClient();
+var response = await client.GetAsync("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam");
+var content = await response.Content.ReadAsStringAsync();
+File.WriteAllText("time.json", content);
